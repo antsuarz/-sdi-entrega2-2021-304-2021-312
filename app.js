cec9e3c39d48ca46bd52dd4c750f444a2d0b8394
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-05-04 18:51:06 +0200

    Cambios en la aplicacion(barra de navegación con dinero y usuario), agregar usuarios, agregar ofertas cada usuario, ver ofertas propias)

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 let express = require('express');
 let app = express();
 
+let expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+let crypto = require('crypto');
+let fileUpload = require('express-fileupload');
+app.use(fileUpload());
 let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.jxgw2.mongodb.net:27017,tiendamusica-shard-00-01.jxgw2.mongodb.net:27017,tiendamusica-shard-00-02.jxgw2.mongodb.net:27017/myWallapop?ssl=true&replicaSet=atlas-od6pn1-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 
-app.use(express.static('public'));
 
 //Rutas
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rofertas.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rofertas.js")(app, swig, gestorBD);
 
 
 app.listen(app.get('port'), function(){
     console.log('servidor activo');
 });
+
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+app.use("/ofertas/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+
+app.use(express.static('public'));
--- routes/rofertas.js
+++ routes/rofertas.js
@@ -1,57 +1,91 @@
-module.exports = function(app, swig, mongo) {
+module.exports = function(app, swig, gestorBD) {
     app.get("/ofertas", function(req, res) {
-        let ofertas= [{
-            "nombre": "cosa",
-            "descripcion": "una cosa",
-            "precio": "2"
-        }, {
-            "nombre": "cosa2",
-            "descripcion": "otra cosa",
-            "precio": "2"
-        }];
+
 
         let respuesta = swig.renderFile('views/btienda.html', {
-            vendedor : 'Tienda',
+            user: req.session.usuario,
             ofertas : ofertas
         });
         res.send(respuesta);
     });
     app.get('/ofertas/agregar', function (req, res) {
-        let respuesta = swig.renderFile('views/bagregar.html', {
 
+        let respuesta = swig.renderFile('views/bagregar.html', {
+            user: req.session.usuario,
+            dinero: req.session.dinero
         });
         res.send(respuesta);
     })
 
-    app.get('/ofertas/:id', function(req, res) {
-        let respuesta = 'id: ' + req.params.id;
-        res.send(respuesta);
+
+    app.get('/oferta/:id', function (req, res) {
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.obtenerOfertas(criterio,function(ofertas){
+            if ( ofertas == null ){
+                res.send("Error al recuperar la oferta.");
+            } else {
+                let respuesta = swig.renderFile('views/boferta.html',
+                    {
+                        oferta : ofertas[0],
+                        user: req.session.usuario,
+                        dinero: req.session.dinero
+                    });
+                res.send(respuesta);
+            }
+        });
     });
 
     app.post("/ofertas", function (req, res) {
+
+
         var date = new Date();
         var fechaString = date.getDay() + "/" +date.getMonth() + "/"+ date.getFullYear()+" Hora: " + date.getHours()+":"+date.getMinutes();
         let oferta = {
             nombre: req.body.nombre,
             detalles: req.body.detalles,
             fecha: fechaString,
+            autor: req.session.usuario,
             precio:req.body.precio
         }
         // Conectarse
-        mongo.MongoClient.connect(app.get('db'), function(err, db) {
-            if (err) {
-                res.send("Error de conexión: " + err);
+        gestorBD.insertarOferta(oferta, function(id){
+            if (id == null) {
+                res.send("Error al insertar la oferta");
             } else {
-                let collection = db.collection('ofertas');
-                collection.insertOne(oferta, function(err, result) {
-                    if (err) {
-                        res.send("Error al insertar la oferta " + err);
-                    } else {
-                        res.send("oferta agregada con id: "+ result.ops[0]._id);
-                    }
-                    db.close();
-                });
+                if (req.files.foto != null) {
+                    var imagen = req.files.foto;
+                    imagen.mv('public/fotos/' + id + '.png', function(err) {
+                        if (err) {
+                            res.send("Error al subir la foto");
+                        } else {
+                            res.send("Agregada id: " + id);
+                        }
+                    });
+                }
+
             }
         });
     });
+
+    app.get("/tienda", function(req, res) {
+        let criterio = {};
+        if( req.query.busqueda != null ){
+            criterio = { "nombre" :  {$regex : ".*"+req.query.busqueda+".*"} };
+        }
+
+        gestorBD.obtenerOfertas( criterio,function(ofertas) {
+            if (ofertas == null) {
+                res.send("Error al listar ");
+            } else {
+                let respuesta = swig.renderFile('views/btienda.html',
+                    {
+                        ofertas : ofertas,
+                        user: req.session.usuario,
+                        dinero: req.session.dinero
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+
 };
