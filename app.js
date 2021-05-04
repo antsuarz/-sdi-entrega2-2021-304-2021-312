d5c965073504f30d1a37f1a4a49795313d9b1780
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-05-04 16:10:15 +0200

    Base de datos creada (se pueden añadir las ofertas)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -8,12 +9,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.set('port', 8081);
+app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.jxgw2.mongodb.net:27017,tiendamusica-shard-00-01.jxgw2.mongodb.net:27017,tiendamusica-shard-00-02.jxgw2.mongodb.net:27017/myWallapop?ssl=true&replicaSet=atlas-od6pn1-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 app.use(express.static('public'));
 
 //Rutas
 require("./routes/rusuarios.js")(app, swig);
-require("./routes/rofertas.js")(app, swig);
+require("./routes/rofertas.js")(app, swig, mongo);
 
 
 app.listen(app.get('port'), function(){
--- routes/rofertas.js
+++ routes/rofertas.js
@@ -1,4 +1,4 @@
-module.exports = function(app, swig) {
+module.exports = function(app, swig, mongo) {
     app.get("/ofertas", function(req, res) {
         let ofertas= [{
             "nombre": "cosa",
@@ -10,16 +10,48 @@ module.exports = function(app, swig) {
             "precio": "2"
         }];
 
-        let respuesta = swig.renderFile('views/tienda.html', {
+        let respuesta = swig.renderFile('views/btienda.html', {
             vendedor : 'Tienda',
             ofertas : ofertas
         });
+        res.send(respuesta);
+    });
+    app.get('/ofertas/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+
+    app.get('/ofertas/:id', function(req, res) {
+        let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
 
     app.post("/ofertas", function (req, res) {
         var date = new Date();
         var fechaString = date.getDay() + "/" +date.getMonth() + "/"+ date.getFullYear()+" Hora: " + date.getHours()+":"+date.getMinutes();
-        res.send("Oferta agregada: "+req.body.nombre + "<br>" + " Detalles: "+ req.body.detalles + "<br>" + "Fecha: "+ fechaString + "<br>" + " Precio: "+ req.body.precio + "<br>");
+        let oferta = {
+            nombre: req.body.nombre,
+            detalles: req.body.detalles,
+            fecha: fechaString,
+            precio:req.body.precio
+        }
+        // Conectarse
+        mongo.MongoClient.connect(app.get('db'), function(err, db) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                let collection = db.collection('ofertas');
+                collection.insertOne(oferta, function(err, result) {
+                    if (err) {
+                        res.send("Error al insertar la oferta " + err);
+                    } else {
+                        res.send("oferta agregada con id: "+ result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
     });
 };
