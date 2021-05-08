081db62d53cb032ef1cc7a0dd864a518aded94c5
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-05-08 22:13:34 +0200

    S3 hecho por completo, S4 funciona pero falta implementar una vista donde ver las conversaciones

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -156,7 +156,7 @@ module.exports = {
             }
         });
     },
-    eliminarUsuario: function (criterio, funcionCallback) {
+    eliminarUsuario: function(criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
                 funcionCallback(null);
@@ -189,6 +189,74 @@ module.exports = {
                             }
                             db.close();
                         });
+                });
+            }
+        });
+    },
+    insertarConversacion : function(conversacion, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('conversaciones');
+                collection.insert(conversacion, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    obtenerConversacion : function(criterio, funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('conversaciones');
+                collection.find(criterio).toArray(function(err, conversacion) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(conversacion);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    insertarMensaje : function(mensaje, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('mensajes');
+                collection.insert(mensaje, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
+    obtenerMensajes : function(criterio,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('mensajes');
+                collection.find(criterio).toArray(function(err, mensajes) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(mensajes);
+                    }
+                    db.close();
                 });
             }
         });
